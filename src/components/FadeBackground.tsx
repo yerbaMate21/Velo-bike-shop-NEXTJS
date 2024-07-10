d296724995f33d66090763eee1a849554d32a724
Rafal Sereda <seredarafal@gmail.com>
2024-07-10 13:56:27 +0200

    update style

--- src/components/FadeBackground.tsx
+++ src/components/FadeBackground.tsx
@@ -5,7 +5,7 @@ interface Props {
 const FadeBackground = ({ active }: Props) => {
   return (
     <div
-      className={`${active && "fixed inset-0 -z-50 flex justify-center bg-black/80"}`}
+      className={`${active && "fixed inset-0 -z-50 flex justify-center bg-black/80 transition-all duration-300"}`}
     />
   );
 };
